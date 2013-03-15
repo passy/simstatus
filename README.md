6522f1640a9b8df2a8bc7d50478c9011f8f26f96
Pascal Hartig <phartig@rdrei.net>
2013-03-15 18:32:43 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Simstatus for Android
+
+Android app available soon from [Google Play](https://play.google.com/store/apps/details?id=net.rdrei.android.simstatus).
---
+++ app/src/main/java/net/rdrei/android/simstatus/ui/MainActivity.java
@@ -0,0 +1,15 @@
+package net.rdrei.android.simstatus.ui;
+
+import net.rdrei.android.simstatus.R;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		setContentView(R.layout.main);
+	}
+}
