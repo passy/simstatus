0eb7d8b01e91f731d86d2f8a0c5a9b7d9fec0e38
Pascal Hartig <passy@twitter.com>
2014-03-30 19:50:55 +0100

    Use @daithiocrualaoich's Option and fix ads

--- simstatus/src/main/java/net/rdrei/android/simstatus/ui/AdViewManagerImpl.java
+++ simstatus/src/main/java/net/rdrei/android/simstatus/ui/AdViewManagerImpl.java
@@ -3,9 +3,17 @@ package net.rdrei.android.simstatus.ui;
 import net.rdrei.android.simstatus.Constants;
 import net.rdrei.android.simstatus.R;
 import android.app.Activity;
+import android.view.InflateException;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.crashlytics.android.Crashlytics;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.gu.option.Function;
+import com.gu.option.Option;
+import com.gu.option.UnitFunction;
 
 /**
  * Loads and injects an AdView if required into an existing layout.
@@ -23,8 +31,35 @@ public class AdViewManagerImpl implements AdViewManager {
 		mInflater = activity.getLayoutInflater();
 	}
 
-	private View getAdView() {
-		return mInflater.inflate(R.layout.adview, null, false);
+	private Option<View> getAdView(final ViewGroup baseView) {
+		try {
+			return Option.some(mInflater.inflate(R.layout.adview, baseView, false));
+		} catch (InflateException exc) {
+			Crashlytics.logException(exc);
+		}
+
+		return Option.none();
+	}
+
+	private Option<AdView> setupView(final View view) {
+		AdView adView;
+
+		try {
+			adView = (AdView) view;
+		} catch (ClassCastException err) {
+			adView = null;
+			Crashlytics.logException(err);
+		}
+
+		if (adView != null) {
+			final AdRequest adRequest = new AdRequest.Builder()
+					.addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
+					.build();
+			adView.loadAd(adRequest);
+			return Option.some(adView);
+		}
+
+		return Option.none();
 	}
 
 	/**
@@ -34,11 +69,20 @@ public class AdViewManagerImpl implements AdViewManager {
 	 *            The layout element to add the view to.
 	 */
 	@Override
-    public void addToView(final ViewGroup baseView) {
-		final View adView = getAdView();
-		baseView.addView(adView);
+	public void addToView(final ViewGroup baseView) {
+		final Option<View> adView = getAdView(baseView);
+		adView.flatMap(new Function<View, Option<AdView>>() {
+			@Override
+			public Option<AdView> apply(View v) {
+				return setupView(v);
+			}
+		}).foreach(new UnitFunction<AdView>() {
+			@Override
+			public void apply(AdView v) {
+				baseView.addView(v);
+			}
+		});
 	}
-
 	/**
 	 * Add to the given baseView, but only if the application is non-adfree.
 	 * 
